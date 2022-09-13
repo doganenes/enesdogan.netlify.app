29b3ab9d717656ffb3423ec4d4e7808eab2b342d
Enes Doğan <enesdoganlive@hotmail.com>
2022-09-13 22:11:51 +0300

    Netlify Portfolio | .content-1 completed

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-let icon = document.getElementById(".icon");
+// addEventListener Selectors
+let link1 = "https://todoapp-website.netlify.app/";
+let link2 = "https://medium-clone-bootstrap.vercel.app/";
+let link3 = "https://kitchen-menu-website.netlify.app/";
+let link4 = "https://personal-portfolio-website-project.netlify.app/";
+let link5 = "https://technical-documentation-page-project.netlify.app/";
+let link6 = "https://survey-form-project.vercel.app/";
 
-function handUp() {
-  icon.style.transform = transLateY(10);
-}
+let card1 = document.querySelector(".card-1");
+let card2 = document.querySelector(".card-2");
+let card3 = document.querySelector(".card-3");
+let card4 = document.querySelector(".card-4");
+let card5 = document.querySelector(".card-5");
+let card6 = document.querySelector(".card-6");
 
-setInterval(handUp, 1000);
+// eventListeners
+card1.addEventListener("click", () => {
+  window.open(link1);
+});
+card2.addEventListener("click", () => {
+  window.open(link2);
+});
+card3.addEventListener("click", () => {
+  window.open(link3);
+});
+card4.addEventListener("click", () => {
+  window.open(link4);
+});
+card5.addEventListener("click", () => {
+  window.open(link5);
+});
+card6.addEventListener("click", () => {
+  window.open(link6);
+});
